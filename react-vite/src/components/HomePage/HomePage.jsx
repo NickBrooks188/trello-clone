263b024351275b1931d93adb357f02f686e751cf
Nick Brooks <nicholasbrooks1@msn.com>
2024-01-04 13:59:48 -0800

    Update HomePage.jsx

--- react-vite/src/components/HomePage/HomePage.jsx
+++ react-vite/src/components/HomePage/HomePage.jsx
@@ -1,5 +1,13 @@
+import { useSelector } from 'react-redux'
+import { Navigate } from 'react-router-dom';
 import './HomePage.css'
 
 export default function HomePage() {
+
+    const sessionUser = useSelector(state => state.session.user)
+
+    if (!sessionUser) return <Navigate to="/" replace={true} />;
+
+
     return "Home page"
 }
