021ebc1575671abfa5db5bd9b5e8bd1097f38774
Nick Brooks <nicholasbrooks1@msn.com>
2024-01-04 10:27:11 -0800

    Added top navbar

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,18 +1,34 @@
 import { NavLink } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
+import { useSelector } from "react-redux";
+import OpenModalMenuItem from "./OpenModalMenuItem";
+import SignupFormModal from "../SignupFormModal";
+import LoginFormModal from "../LoginFormModal";
 
 function Navigation() {
+  const sessionUser = useSelector(state => state.session.user)
   return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-
-      <li>
-        <ProfileButton />
-      </li>
-    </ul>
+    <div className="top-navbar">
+      <NavLink to="/">Logo</NavLink>
+      <div className="top-navbar-rhs-wrapper">
+        {sessionUser && (
+          <ProfileButton />
+        )}
+        {!sessionUser && (
+          <div className="login-signup-wrapper">
+            <OpenModalMenuItem
+              itemText="Log In"
+              modalComponent={<LoginFormModal />}
+            />
+            <OpenModalMenuItem
+              itemText="Sign Up"
+              modalComponent={<SignupFormModal />}
+            />
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
 
